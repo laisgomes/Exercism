bf0c1f07ab5a7d060994161934b2d1bc4c6e9604
Lais Gomes <laisgomes2107@gmail.com>
2018-09-16 22:31:55 -0300

    Add extractRowFromOneNumberMatrixTest code

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -1,12 +1,18 @@
 
 class Matrix {
 
+    private final String matrixAsString;
+
     Matrix(String matrixAsString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.matrixAsString = matrixAsString;
     }
 
     int[] getRow(int rowNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if(rowNumber == 0){
+            int[] row = {1};
+            return row;
+        }
+        return null;
     }
 
     int[] getColumn(int columnNumber) {
