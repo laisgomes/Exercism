9f4b31669b72d667e214ae879f23b4f8b01ac245
Lais Gomes <laisgomes2107@gmail.com>
2018-09-17 22:22:38 -0300

    Add getRow method

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -1,21 +1,38 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Matrix {
 
     private final String matrixAsString;
 
+    private int[][] row;
+
+    private int[] column;
+
     Matrix(String matrixAsString) {
         this.matrixAsString = matrixAsString;
+        this.row = buildRow(this.matrixAsString);
     }
 
-    int[] getRow(int rowNumber) {
-        if(rowNumber == 0){
-            int[] row = {1};
-            return row;
+    int[][] buildRow(String matrixAsString){
+        String[] matrix = matrixAsString.split("\n");
+
+        final int[][] ints = new int[matrix.length][];
+        for (int i=0; i < matrix.length; i++) {
+            String[] splited = matrix[i].split(" ");
+            ints[i] = Arrays.stream(splited).mapToInt(Integer::parseInt).toArray();
         }
+        return ints;
+    }
+    int[] buildCollumn(String matrixAsString){
         return null;
+    }
+    int[] getRow(int rowNumber) {
+       return this.row[rowNumber];
     }
 
     int[] getColumn(int columnNumber) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
+
 }
--- java/matrix/src/test/java/MatrixTest.java
+++ java/matrix/src/test/java/MatrixTest.java
@@ -23,7 +23,6 @@ public class MatrixTest {
 		assertArrayEquals(expectedRow, matrix.getRow(rowIndex));
 	}
 	
-	@Ignore("Remove to run test")
 	@Test
 	public void extractRowFromMatrixTest() {
 		String matrixAsString = "1 2\n3 4";
@@ -35,7 +34,6 @@ public class MatrixTest {
 		assertArrayEquals(expectedRow, matrix.getRow(rowIndex));
 	}
 	
-	@Ignore("Remove to run test")
 	@Test
 	public void extractRowFromDiffWidthsMatrixTest() {
 		String matrixAsString = "1 2\n10 20";
@@ -47,7 +45,6 @@ public class MatrixTest {
 		assertArrayEquals(expectedRow, matrix.getRow(rowIndex));
 	}
 	
-	@Ignore("Remove to run test")
 	@Test
 	public void extractRowFromNonSquareMatrixTest() {
 		String matrixAsString = "1 2 3\n4 5 6\n7 8 9\n8 7 6";
