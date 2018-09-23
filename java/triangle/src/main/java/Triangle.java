a18b7fe49b8b66bb853e838a2fd4f0b5dbc2de35
Lais Gomes <laisgomes2107@gmail.com>
2018-09-23 08:26:12 -0300

    Add triangle solution for  equilateralTrianglesHaveEqualSides

---
+++ java/triangle/src/main/java/Triangle.java
@@ -0,0 +1,28 @@
+class Triangle {
+
+    private final double side1;
+    private final double side2;
+    private final double side3;
+
+    Triangle(double side1, double side2, double side3) throws TriangleException {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    boolean isEquilateral() {
+        if(side1 == side2 && side2 == side3){
+            return true;
+        }
+        return false;
+    }
+
+    boolean isIsosceles() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    boolean isScalene() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
