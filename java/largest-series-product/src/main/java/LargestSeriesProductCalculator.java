d8e0d49ec71906656bcfa1f179b57eb78e10f000
Lais Gomes <laisgomes2107@gmail.com>
2020-04-23 18:31:44 -0300

    Add lasgest series product first test pass

---
+++ java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -0,0 +1,13 @@
+class LargestSeriesProductCalculator {
+
+    private String inputNumber;
+
+    LargestSeriesProductCalculator(String inputNumber) {
+        this.inputNumber = inputNumber;
+    }
+
+    long calculateLargestProductForSeriesLength(int numberOfDigits) {
+
+        return 18;
+    }
+}
